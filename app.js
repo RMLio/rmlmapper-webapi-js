ebbb32c93e6dd705c029e9b684af0e2bf910cdbe
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-02-01 10:21:40 +0100

    update accept header

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
