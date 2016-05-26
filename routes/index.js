aa94de93b7986a3e04d708331fde914127b93c57
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-05-26 15:40:37 +0200

    fixed calling validator

--- routes/index.js
+++ routes/index.js
@@ -165,28 +165,20 @@ router.get('/downloadfile', function (req, res){
   });
 });
 
-router.get('/api/v1/validate', function (req, res){
+router.post('/api/v1/validate', function (req, res){
+  var options = {
+    method: 'post',
+    form: req.body,
+    url: config.urls.validator + '/api/v1/validate'
+  };
 
-  var body = "";
-
-  req.on('data', function (chunk) {
-    body += chunk;
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    } else {
+      res.status(400).send({error: error});
+    }
   });
-
-  req.on('end', function () {
-    var options = {
-      body: body,
-      url: config.urls.validator + '/api/v1/validate'
-    };
-
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      } else {
-        res.status(400).send({error: error});
-      }
-    });
-  })
 });
 
 module.exports = router;
